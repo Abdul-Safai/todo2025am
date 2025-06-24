a810f7a248126bcf38ea5fd6dd43d9345ae1aa66
Abdul <abdulaziz_safi@yahoo.com>
2025-06-24 09:22:17 -0400

    Initial TodoList Implemented

---
+++ todo/src/app/app.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { TodoList }  from './todoList';
+import { TodoItem } from './todoItem';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'todo';
+
+  private list  = new TodoList("Abdul", [
+    new TodoItem("go for a run", true),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect Tickets")
+  ]);
+
+  get username(): string 
+  {
+    return this.list.user;
+  }
+
+  get itemCount(): number{
+    return this.list.items. filter(item => !item.complete) . length;
+  }
+}
